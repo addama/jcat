98afadcb8bc47a584d3ef8012780da4fe9bca62f
Addama <addama.sanders@gmail.com>
2018-06-20 16:20:16 -0700

    Added sqlite so I can play with that too

--- src/main/java/net/addama/jcat/DatabaseHandler.java
+++ src/main/java/net/addama/jcat/DatabaseHandler.java
@@ -1,51 +1,18 @@
 // http://hsqldb.org/doc/2.0/guide/index.html
 package jcat;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.HashMap;
 
 public class DatabaseHandler {
-	private static final String driver = "org.hsqldb.jdbc.JDBCDriver";
-	private static final String url = "jdbc:hsqldb:file:db;shutdown-true";
+	private static final String driver = "org.sqlite.JDBC";
+	private static final String url = "jdbc:sqlite:database.db";
 	private static final String user = "sa";
 	private static final String pass = "";
 	private static final HashMap<String, String> sql = new HashMap<String, String>();
 	static {
 		sql.put("test", "select");
-		sql.put("init", "drop table item;\r\n" + 
-				"create table item(\r\n" + 
-				"	-- basic identity\r\n" + 
-				"	id integer,\r\n" + 
-				"	name varchar(100),\r\n" + 
-				"	-- slot, slot level\r\n" + 
-				"	slot varchar(50),\r\n" + 
-				"	slotlevel tinyint,\r\n" + 
-				"	-- group, subgroup\r\n" + 
-				"	group1 varchar(50),\r\n" + 
-				"	group2 varchar(50),\r\n" + 
-				"	-- misc data\r\n" + 
-				"	brand varchar(50),\r\n" + 
-				"	notes varchar(250),\r\n" + 
-				"	created timestamp,\r\n" + 
-				"	purchased timestamp,\r\n" + 
-				"	price decimal,\r\n" + 
-				"	formality varchar(25),\r\n" + 
-				"	material varchar(50),\r\n" + 
-				"	warmth varchar(25),\r\n" + 
-				"	fit varchar(25),\r\n" + 
-				"	size varchar(5),\r\n" + 
-				"	sleevelength varchar(25),\r\n" + 
-				"	pattern varchar(25),\r\n" + 
-				"	-- colors\r\n" + 
-				"	color1 varchar(50),\r\n" + 
-				"	color2 varchar(50),\r\n" + 
-				"	color3 varchar(50)\r\n" + 
-				")");
+		sql.put("init", "drop table item;\r\n");
 	}
 
 	private Connection connection;
